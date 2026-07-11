62a5ba58772d060db54c7237e96562a85277cc6c
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-07-11 23:09:57 +0600

    create reusable container component

---
+++ src/app/components/common/Container.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import clsx from "clsx";
+
+interface ContainerProps {
+    children: ReactNode;
+    className?: string;
+}
+
+const Container = ({ children, className }: ContainerProps) => {
+    return (
+        <div
+            className={clsx(
+                "mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-8",
+                className
+            )}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default Container;
