b0ac692e87828311b70862d1278c0239daf7898f
Najmun Nahar Anjum <najmunnanjum121@gmail.com>
2026-07-15 09:59:42 +0600

    url changed

--- src/app/lib/auth-client.ts
+++ src/app/lib/auth-client.ts
@@ -2,8 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { jwtClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
-
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     plugins: [
         jwtClient(),
 
